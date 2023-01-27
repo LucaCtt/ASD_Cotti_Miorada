0b4e018f79a0a71575e4a5c76bd46c3606b14d5d
Luca Cotti <lucacotti@outlook.com>
2023-01-27 18:49:41 +0100

    Adds zero cols fixing

--- src/gen.py
+++ src/gen.py
@@ -11,7 +11,9 @@ import numpy as np
 class Inst:
     """Represents an instance of the EC problem."""
     input_matrix: np.ndarray
-    prob: float
+    prob: float 
+    guarantee_sol: bool = False
+    fixed_zero_col: bool = False
     gen_at: datetime = datetime.today()
 
 
@@ -24,15 +26,13 @@ def gen_inst(card_m: int, card_n: int, prob: float, guarantee_sol: bool) -> Inst
         prob (float): The probability of a bit to be 1.
         guarantee_sol (bool): True if the instance must have at least one solution.
 
-    Raises:
-        ValueError: _description_
 
     Returns:
-        Inst: _description_
+        Inst: The generated instance.
     """
 
     # There are at most 2^M unique rows,
-    # so if N >= 2^M it is not possible to generate all different rows
+    # so if N >= 2^M it is not possible to generate all different rows.
     if card_n >= 2**card_m:
         raise ValueError('N must be less than 2^M')
 
@@ -41,9 +41,9 @@ def gen_inst(card_m: int, card_n: int, prob: float, guarantee_sol: bool) -> Inst
     # Where to start the random generation.
     # If a solution must be guaranteed the first m rows
     # will contain an identity matrix,
-    # so generation will start from the m+1 row
+    # so generation will start from the m+1 row.
     start_index = 0
-    if guarantee_sol:
+    if guarantee_sol and card_m <= card_n:
         start_index = card_m
         input_matrix[0:card_m] = np.eye(card_m, dtype=int)
 
@@ -64,7 +64,18 @@ def gen_inst(card_m: int, card_n: int, prob: float, guarantee_sol: bool) -> Inst
 
         input_matrix[i] = row
 
-    return Inst(input_matrix, prob)
+    fixed_zero_col = False
+    # Check that there are no empty columns,
+    # which would make the problem unsolvable.
+    # If there are, for each empty column a random row is chosen
+    # and the corresponding bit is set to 1.
+    empty_idxs = np.argwhere(np.all(input_matrix[..., :] == 0, axis=0))
+    if empty_idxs.size > 0:
+        fixed_zero_col = True
+        for idx in empty_idxs:
+            input_matrix[np.random.randint(input_matrix.shape[0], size=1), idx] = 1
+
+    return Inst(input_matrix, prob, guarantee_sol, fixed_zero_col)
 
 
 def write_inst(output_file: str, inst: Inst):
@@ -81,7 +92,9 @@ def write_inst(output_file: str, inst: Inst):
             f';;; Cardinality of M: {str(inst.input_matrix.shape[1])}\n')
         file.write(
             f';;; Cardinality of N: {str(inst.input_matrix.shape[0])}\n')
-        file.write(f';;; Probability: {str(inst.prob)}')
+        file.write(f';;; Probability: {str(inst.prob)}\n')
+        file.write(f';;; Guarantee solution: {str(inst.guarantee_sol)}\n')
+        file.write(f';;; Fixed zero col: {str(inst.fixed_zero_col)}')
 
         for row in inst.input_matrix:
             file.write(f'\n{np.array2string(row)[1:-1]} -')
